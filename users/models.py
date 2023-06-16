27f9e3bec9e1b2061e85f9310429c09e013d72c3
Greg-GJE <gjegreg@gmail.com>
2023-06-16 07:54:34 +0530

    Implemented login functionality

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
+"""
+hELLO
+"""
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='Profile')
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    class Meta:
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
+
+    def __str__(self) -> str:
+        return f'{self.user.username} Profile'
--- users/views.py
+++ users/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 from . import constants
 
@@ -9,7 +13,25 @@ def index(request):
 
 def login_user(request):
     if request.method == constants.POST:
-        pass
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user_model = get_user_model()
+
+        if not user_model.objects.filter(username = username).exists():
+            messages.error(request, 'Invalid Username')
+            return redirect(reverse('users:login'))
+
+        user = authenticate(username = username, password = password)
+
+        if user is None:
+            messages.error(request, 'Invalid Password')
+            return redirect(reverse('users:login'))
+        else:
+            login(request, user)
+            return redirect(reverse('news:index'))
+
+
     elif request.method == constants.GET:
         return render(request, 'users/login.html')
     return None
@@ -17,7 +39,34 @@ def login_user(request):
 
 def register_user(request):
     if request.method == constants.POST:
-        pass
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user_model = get_user_model()
+
+        existing_user_by_username = user_model.objects.filter(username = username)
+
+        if existing_user_by_username.exists():
+            messages.error(request, 'Username already taken')
+            return redirect(reverse('users:register'))
+
+        # checking user by email
+        existing_user_by_email = user_model.objects.filter(email = email)
+
+        if existing_user_by_email.exists():
+            messages.error(request, 'Email already exists. Try to login')
+            return redirect(reverse('users:register'))
+
+        # now try to save the user
+
+
     elif request.method == constants.GET:
         return render(request, 'users/register.html')
     return None
+
+def logout_user(request):
+    logout(request)
+    return redirect(reverse('news:index'))
