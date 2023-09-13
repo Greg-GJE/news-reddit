70691b1ca636056701360b817d7a25a51b5b2495
Greg-GJE <gjegreg@gmail.com>
2023-09-13 20:53:42 +0530

    Fixed issue for heroku auto remove media files

--- users/signals.py
+++ users/signals.py
@@ -3,6 +3,10 @@ from django.contrib.auth import get_user_model
 from django.dispatch import receiver
 from .models import Profile
 
+import logging
+
+logger = logging.getLogger("herokulogger")
+
 user_model = get_user_model()
 
 @receiver(post_save, sender=user_model)
@@ -13,4 +17,8 @@ def create_profile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=user_model)
 def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    try:
+        instance.profile.save()
+    except FileNotFoundError as error:
+        logger.error("Profile found removed by the server %s", error)
+
